17531e3ce0df4bf026d86a1c84c706585f58418f
Derek Poon <derekp@ece.ubc.ca>
2016-01-22 16:28:33 -0800

    Ensure FQDN is in $HOSTNAME and $DOMAIN (again)
    Dracut's DHCP client sets $HOSTNAME to include $DOMAIN.  However,
    traditionally, $HOSTNAME is just the node name.  Therefore, if $HOSTNAME
    contains a dot, we need to split it into a $HOSTNAME and a $DOMAIN.
    
    This needs to happen early, _before_ attempting to fetch host keys.

--- hooks/setup.FAIBASE.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$DOMAIN" ]; then
-    case "$HOSTNAME" in
-        *.*)
-            DOMAIN="${HOSTNAME#*.}"
-            HOSTNAME="${HOSTNAME%%.*}"
-            export DOMAIN HOSTNAME
-            ;;
-    esac
-fi
